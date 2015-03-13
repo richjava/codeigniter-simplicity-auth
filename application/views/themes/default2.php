2ea9857745866049332f1771aeea9afb6d44a15d
richard.lovell <richard.lovell@45021-STAFF.christchurch.natcoll.ac.nz>
2015-03-13 15:30:33 +1300

    Updated dialog

--- application/views/themes/default2.php
+++ application/views/themes/default2.php
@@ -21,92 +21,99 @@
     </head>
     <body id="page">
         <div class="container">
-        <div class="tail-top-right"></div>
-        <div id="main">
-            <!-- header -->
-            <div id="header"></div>
-            <!-- content -->
-            <div id="content">
-                <div class="wrapper">
-                    <div class="col-1">
-                        <div class="box">
-                            <div class="inner">
-                                <div class="title"><a href="http://netbeans.org/features/php/">NetBeans IDE PHP</a></div>
-                                <ul class="list">
-                                    <li><span><a href="<?php echo site_url('auth'); ?>">Home</a></span></li>
-                                    <li><span><a href=""
-                                                 ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/PENDING.png" alt="" title="Pending TODOs" class="icon" />Pending TODOs</a></span></li>
-                                    <li><span><a href=""
-                                                 ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/DONE.png" alt="" title="Done TODOs" class="icon" />Done TODOs</a></span></li>
-                                    <li><span><a href=""
-                                                 ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/VOIDED.png" alt="" title="Voided TODOs" class="icon" />Voided TODOs</a></span></li>
-                                    <li class="last"><span><a href=""
-                                                              ><img src="<?php echo base_url(); ?>assets/themes/default/images/action/add.png" alt="" title="Add TODO" class="icon" />Add TODO</a></span></li>
-                                </ul>
+            <div class="tail-top-right"></div>
+            <div id="main">
+                <!-- header -->
+                <div id="header"></div>
+                <!-- content -->
+                <div id="content">
+                    <div class="wrapper">
+                        <div class="col-1">
+                            <div class="box">
+                                <div class="inner">
+                                    <div class="title"><a href="http://netbeans.org/features/php/">NetBeans IDE PHP</a></div>
+                                    <ul class="list">
+                                        <li><span><a href="<?php echo site_url('auth'); ?>">Home</a></span></li>
+                                        <li><span><a href=""
+                                                     ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/PENDING.png" alt="" title="Pending TODOs" class="icon" />Pending TODOs</a></span></li>
+                                        <li><span><a href=""
+                                                     ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/DONE.png" alt="" title="Done TODOs" class="icon" />Done TODOs</a></span></li>
+                                        <li><span><a href=""
+                                                     ><img src="<?php echo base_url(); ?>assets/themes/default/images/status/VOIDED.png" alt="" title="Voided TODOs" class="icon" />Voided TODOs</a></span></li>
+                                        <li class="last"><span><a href=""
+                                                                  ><img src="<?php echo base_url(); ?>assets/themes/default/images/action/add.png" alt="" title="Add TODO" class="icon" />Add TODO</a></span></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="indent">
+                                <div style="text-align: right;">
+                                    <?php if ($this->session->userdata('is_logged_in')): ?>
+
+
+                                        <a href="<?php echo site_url('auth/logout'); ?>">Sign out</a>
+                                    <?php else: ?>
+                                        <!--                                ajax login modal dialog-->
+                                        <a href="#" class="btn btn-lg btn-success"
+                                           data-toggle="modal"
+                                           data-target="#basicModal">Sign in</a>
+
+
+                                    <?php endif; ?>
+                                </div>
+                                <?php echo $output; ?>
                             </div>
                         </div>
                     </div>
-                    <div class="col-2">
-                        <div class="indent">
-                            <div style="text-align: right;">
-                                <?php if ($this->session->userdata('is_logged_in')): ?>
-
-
-                                    <a href="<?php echo site_url('auth/logout'); ?>">Sign out</a>
-                                <?php else: ?>
-                                    <!--                                ajax login modal dialog-->
-                                    <a href="#" class="btn btn-lg btn-success"
-                                       data-toggle="modal"
-                                       data-target="#basicModal">Sign in</a>
-
-
-                                <?php endif; ?>
-                            </div>
-                            <?php echo $output; ?>
+                </div>
+                <!-- footer -->
+                <div id="footer">
+                    <div class="indent">
+                        <div class="fleft"><a href="https://blogs.oracle.com/netbeansphp/">NetBeans PHP team</a>, 2011 &copy; Copyright Oracle corp., All rights reserved</div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="myModalLabel">Sign In</h4>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <!-- footer -->
-            <div id="footer">
-                <div class="indent">
-                    <div class="fleft"><a href="https://blogs.oracle.com/netbeansphp/">NetBeans PHP team</a>, 2011 &copy; Copyright Oracle corp., All rights reserved</div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Sign In</h4>
-                    </div>
-                    <div class="modal-body">
-                    <form action="auth/validate">
+                        <form <form onsubmit="login()">
+                        <div class="modal-body">
+                            
 
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input class="form-control" name="username"/>    
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Username</label>
-                                <input class="form-control" name="password" type="password"/>
-                            </div>
-                            <div class="wrapper" style="text-align:right;">
-                                <input class="btn btn-default" name="submit" type="submit" value="Go"/>
-                            </div>	
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input class="form-control" name="username"/>    
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Username</label>
+                                    <input class="form-control" name="password" type="password"/>
+                                </div>
+                                	
 
-                    </form>
-                        <?php echo "Don't have an account? " . anchor('auth/signup', "Create an account."); ?>
-                    </div>
-                    
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Sign In</button>
+
+                                <?php echo "Don't have an account? " . anchor('auth/signup', "Create an account."); ?>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit"  class="btn btn-primary">Sign In</button>
+                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-        </div>
+        <script type="text/javascript">
+
+            function login() {
+                alert("hello");
+            }
+
+        </script>
     </body>
 </html>
