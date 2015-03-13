8b32e30a1363620a3e49baa75145fbeb322fe9f7
richard.lovell <richard.lovell@45021-STAFF.christchurch.natcoll.ac.nz>
2015-03-13 15:31:20 +1300

    Updated dialog

--- application/views/themes/default2.php
+++ application/views/themes/default2.php
@@ -81,7 +81,7 @@
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                             <h4 class="modal-title" id="myModalLabel">Sign In</h4>
                         </div>
-                        <form <form onsubmit="login()">
+                        <form onsubmit="login()">
                         <div class="modal-body">
                             
 
